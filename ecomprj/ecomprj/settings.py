21a7c2a6c7de01a29eb1f09080acd6e3c8dc2f7a
ibuildsoftware <ibuildsoftware@outlook.fr>
2024-10-30 00:47:11 +0100

    Configure "templates" directory and create its folder

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+# import operating system
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -59,7 +62,7 @@ ROOT_URLCONF = 'ecomprj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # configure Templates directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
