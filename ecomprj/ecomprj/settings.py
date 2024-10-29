73f90a0f9c93b4d621418fa71a17960b3e33866c
ibuildsoftware <ibuildsoftware@outlook.fr>
2024-10-30 00:39:25 +0100

    Install apps in Settings.py

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # Custom Apps = installed apps (components)
+    'core', 
+
 ]
 
 MIDDLEWARE = [
